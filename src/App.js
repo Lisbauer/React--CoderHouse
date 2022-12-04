c8a69e34aa7cf78fc6a1166c86d6e77cbe641032
Lisbauer <lisa.bauer@hotmail.com>
2022-12-03 23:29:32 -0300

    entrega final coderhouse

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Footer from "./components/Footer/Footer";
+import Navmain from "./components/NavBar/NavMain";
+import HomeMaiN from "./components/HomePage/HomeMain";
+
+function App() {
+  return (
+    <>
+      <Navmain />
+      <HomeMaiN />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
